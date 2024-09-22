1349632f75866998b719a37026fe4252969dcb16
sl33pyturtl3 <zmcstro05@gmail.com>
2024-09-22 15:13:35 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)
+    {
+     int checkNum = 3123;
+     if (checkNum % 2 == 0)
+         System.out.println("The number is even");
+     else
+         System.out.println("The number is odd");
+    }
+}
